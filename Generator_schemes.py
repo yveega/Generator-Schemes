2cf2bb7c139e5195db5f733215df1a69961750cb
yveega <veega@gmail.com>
2019-11-05 10:59:50 +0300

    Add interface, can generate many schemes in directory

--- Generator_schemes.py
+++ Generator_schemes.py
@@ -1,6 +1,6 @@
 from tkinter import *
-import subprocess
-import os
+from tkinter import filedialog as fd
+from PIL import Image, ImageDraw, ImageTk
 
 EL_SIZE = 100
 TAB = 5
@@ -27,58 +27,91 @@ class Cell:
         self.x = row * (EL_SIZE + 2 * TAB) + TAB
         self.y = column * (EL_SIZE + 2 * TAB) + TAB
 
-    def drow(self, canvas):
+    def draw_on_image(self, imdraw, im):
         if self.is_el:
             if self.up:
-                canvas.create_line(self.x + EL_SIZE // 2, self.y, self.x + EL_SIZE // 2,
-                                   self.y - TAB, fill="black", width=WIRE_WIDTH)
+                imdraw.line([self.x + EL_SIZE // 2, self.y - 1, self.x + EL_SIZE // 2,
+                                   self.y - TAB - 1], fill="black", width=WIRE_WIDTH)
             if self.right:
-                canvas.create_line(self.x + EL_SIZE, self.y + EL_SIZE // 2, self.x + EL_SIZE + TAB,
-                                   self.y + EL_SIZE // 2, fill="black", width=WIRE_WIDTH)
+                imdraw.line([self.x + EL_SIZE, self.y + EL_SIZE // 2 - 1, self.x + EL_SIZE + TAB,
+                                   self.y + EL_SIZE // 2 - 1], fill="black", width=WIRE_WIDTH)
             if self.down:
-                canvas.create_line(self.x + EL_SIZE / 2, self.y + EL_SIZE, self.x + EL_SIZE / 2,
-                                   self.y + EL_SIZE + TAB, fill="black", width=WIRE_WIDTH)
+                imdraw.line([self.x + EL_SIZE // 2 - 1, self.y + EL_SIZE, self.x + EL_SIZE // 2 - 1,
+                                   self.y + EL_SIZE + TAB], fill="black", width=WIRE_WIDTH)
             if self.left:
-                canvas.create_line(self.x, self.y + EL_SIZE // 2, self.x - TAB,
-                                   self.y + EL_SIZE // 2, fill="black", width=WIRE_WIDTH)
-            self.img = PhotoImage(file=self.img_name)
-            canvas.create_image(self.x, self.y, anchor=NW, image=self.img)
+                imdraw.line([self.x - 1, self.y + EL_SIZE // 2, self.x - TAB - 1,
+                                   self.y + EL_SIZE // 2], fill="black", width=WIRE_WIDTH)
+            img = Image.open(self.img_name)
+            im.paste(img, (self.x, self.y))
         else:
             if self.up:
-                canvas.create_line(self.x + EL_SIZE // 2, self.y + EL_SIZE // 2 + WIRE_WIDTH // 2, self.x + EL_SIZE // 2,
-                                   self.y - TAB, fill="black", width=WIRE_WIDTH)
+                imdraw.line([self.x + EL_SIZE // 2, self.y + EL_SIZE // 2 + WIRE_WIDTH // 2 - 1, self.x + EL_SIZE // 2,
+                                   self.y - TAB - 1], fill="black", width=WIRE_WIDTH)
             if self.right:
-                canvas.create_line(self.x + EL_SIZE // 2 - WIRE_WIDTH // 2, self.y + EL_SIZE // 2, self.x + EL_SIZE + TAB,
-                                   self.y + EL_SIZE // 2, fill="black", width=WIRE_WIDTH)
+                imdraw.line([self.x + EL_SIZE // 2 - WIRE_WIDTH // 2, self.y + EL_SIZE // 2 - 1, self.x + EL_SIZE + TAB,
+                                   self.y + EL_SIZE // 2 - 1], fill="black", width=WIRE_WIDTH)
             if self.down:
-                canvas.create_line(self.x + EL_SIZE // 2, self.y + EL_SIZE // 2 - WIRE_WIDTH // 2, self.x + EL_SIZE / 2,
-                                   self.y + EL_SIZE + TAB, fill="black", width=WIRE_WIDTH)
+                imdraw.line([self.x + EL_SIZE // 2 - 1, self.y + EL_SIZE // 2 - WIRE_WIDTH // 2, self.x + EL_SIZE / 2 - 1,
+                                   self.y + EL_SIZE + TAB], fill="black", width=WIRE_WIDTH)
             if self.left:
-                canvas.create_line(self.x + EL_SIZE // 2 + WIRE_WIDTH // 2, self.y + EL_SIZE // 2, self.x - TAB,
-                                   self.y + EL_SIZE // 2, fill="black", width=WIRE_WIDTH)
+                imdraw.line([self.x + EL_SIZE // 2 + WIRE_WIDTH // 2 - 1, self.y + EL_SIZE // 2, self.x - TAB - 1,
+                                   self.y + EL_SIZE // 2], fill="black", width=WIRE_WIDTH)
 
 
-with open("sch2.txt", 'r') as fi:
-    data = []
-    for line in fi:
-        data.append(line.strip().split('\t'))
+def draw(scheme):
+    cells = []
+    for i in range(len(scheme)):
+        for j in range(len(scheme[i])):
+            cells.append(Cell(scheme[i][j], j, i))
+    W = len(scheme[0]) * (EL_SIZE + TAB * 2)
+    H = len(scheme) * (EL_SIZE + TAB * 2)
+    im = Image.new("RGB", (W, H), (255, 255, 255))
+    draw = ImageDraw.Draw(im)
+    for el in cells:
+        el.draw_on_image(draw, im)
+    del draw
+    return im
 
-scheme = []
 
-for i in range(len(data)):
-    for j in range(len(data[i])):
-        scheme.append(Cell(data[i][j], j, i))
+def read_from_file(file):
+    with open(file, 'r') as fi:
+        data = []
+        for line in fi:
+            data.append(line.strip().split('\t'))
+    return data
 
-W = len(data[0]) * (EL_SIZE + TAB * 2)
-H = len(data) * (EL_SIZE + TAB * 2)
+
+def save():
+    filename = fd.asksaveasfilename(filetypes=(("PNG files", "*.png"), ("All files", "*.*")))
+    im.save(filename)
+
+
+def work():
+    global ph_im, im
+    filename = fd.askopenfilename(filetypes=(("TXT files", "*.txt"), ("All files", "*.*")))
+    scheme = read_from_file(filename)
+    b_open.destroy()
+    label.destroy()
+    im = draw(scheme)
+    ph_im = ImageTk.PhotoImage(im)
+    picture = Label(r, image=ph_im)
+    picture.pack(padx=20, pady=20)
+    b_save = Button(r, text="Экспорт в PNG", font=("Comic Sans", 19, "bold"), bg="#ffdddd", fg="orange",
+                    activebackground="#ffe8e8", activeforeground="orange", command=save)
+    b_save.pack(side=LEFT, padx=20, pady=20)
+    r.update()
+
+
+###################################################
+# main code
 
 r = Tk()
-c = Canvas(r, width=W, height=H, bg="white")
+r.title("Электрические схемы")
+r['bg'] = "#ffddff"
+label = Label(r, text="Выберите файл с электрической схемой", font=("Comic Sans", 15, "bold"), bg="#ffddff", fg="blue")
+b_open = Button(r, text="Открыть", font=("Comic Sans", 19, "bold"), bg="#ddddff", fg="purple",
+                activebackground="#e8e8ff", activeforeground="purple", command=work)
+label.pack(padx=20, pady=20)
+b_open.pack(padx=20, pady=20)
 
-els = []
-
-for el in scheme:
-    el.drow(c)
-
-c.pack()
 r.mainloop()
--- Randomiser_schemes.py
+++ Randomiser_schemes.py
@@ -1,7 +1,79 @@
 import sys
 import random
-sys.setrecursionlimit(30)
+from tkinter import *
+from tkinter import filedialog as fd
+from tkinter import messagebox as mb
+from PIL import Image, ImageDraw, ImageTk
 types_of_els = ["resister", "battery", "switch", "lamp", "lamp", "lamp", "lamp", "switch"]
+EL_SIZE = 100
+TAB = 5
+WIRE_WIDTH = 6
+h_or_v_els = {"battery", "switch", "resister"}
+
+
+class Cell:
+    def __init__(self, s, row, column):
+        data_cell = s.split()
+        self.is_el = data_cell[0] != '-'
+        self.up = bool(int(data_cell[1]))
+        self.right = bool(int(data_cell[2]))
+        self.down = bool(int(data_cell[3]))
+        self.left = bool(int(data_cell[4]))
+        if self.is_el:
+            if data_cell[0] in h_or_v_els:
+                if self.up or self.down:
+                    self.img_name = "Im_el/" + data_cell[0] + "/v.png"
+                else:
+                    self.img_name = "Im_el/" + data_cell[0] + "/h.png"
+            else:
+                self.img_name = "Im_el/" + data_cell[0] + ".png"
+        self.x = row * (EL_SIZE + 2 * TAB) + TAB
+        self.y = column * (EL_SIZE + 2 * TAB) + TAB
+
+    def draw_on_image(self, imdraw, im):
+        if self.is_el:
+            if self.up:
+                imdraw.line([self.x + EL_SIZE // 2, self.y - 1, self.x + EL_SIZE // 2,
+                                   self.y - TAB - 1], fill="black", width=WIRE_WIDTH)
+            if self.right:
+                imdraw.line([self.x + EL_SIZE, self.y + EL_SIZE // 2 - 1, self.x + EL_SIZE + TAB,
+                                   self.y + EL_SIZE // 2 - 1], fill="black", width=WIRE_WIDTH)
+            if self.down:
+                imdraw.line([self.x + EL_SIZE // 2 - 1, self.y + EL_SIZE, self.x + EL_SIZE // 2 - 1,
+                                   self.y + EL_SIZE + TAB], fill="black", width=WIRE_WIDTH)
+            if self.left:
+                imdraw.line([self.x - 1, self.y + EL_SIZE // 2, self.x - TAB - 1,
+                                   self.y + EL_SIZE // 2], fill="black", width=WIRE_WIDTH)
+            img = Image.open(self.img_name)
+            im.paste(img, (self.x, self.y))
+        else:
+            if self.up:
+                imdraw.line([self.x + EL_SIZE // 2, self.y + EL_SIZE // 2 + WIRE_WIDTH // 2 - 1, self.x + EL_SIZE // 2,
+                                   self.y - TAB - 1], fill="black", width=WIRE_WIDTH)
+            if self.right:
+                imdraw.line([self.x + EL_SIZE // 2 - WIRE_WIDTH // 2, self.y + EL_SIZE // 2 - 1, self.x + EL_SIZE + TAB,
+                                   self.y + EL_SIZE // 2 - 1], fill="black", width=WIRE_WIDTH)
+            if self.down:
+                imdraw.line([self.x + EL_SIZE // 2 - 1, self.y + EL_SIZE // 2 - WIRE_WIDTH // 2, self.x + EL_SIZE / 2 - 1,
+                                   self.y + EL_SIZE + TAB], fill="black", width=WIRE_WIDTH)
+            if self.left:
+                imdraw.line([self.x + EL_SIZE // 2 + WIRE_WIDTH // 2 - 1, self.y + EL_SIZE // 2, self.x - TAB - 1,
+                                   self.y + EL_SIZE // 2], fill="black", width=WIRE_WIDTH)
+
+
+def draw(scheme):
+    cells = []
+    for i in range(len(scheme)):
+        for j in range(len(scheme[i])):
+            cells.append(Cell(scheme[i][j], j, i))
+    W = len(scheme[0]) * (EL_SIZE + TAB * 2)
+    H = len(scheme) * (EL_SIZE + TAB * 2)
+    im = Image.new("RGB", (W, H), (255, 255, 255))
+    draw = ImageDraw.Draw(im)
+    for el in cells:
+        el.draw_on_image(draw, im)
+    del draw
+    return im
 
 
 def make_field(sch):
@@ -87,6 +159,15 @@ def randomise(k_els):
     els[-1] = (els[-1], 0, len(saved_conns) - 1)
     saved_conns[0].add(len(els) - 1)
     saved_conns[-1].add(len(els) - 1)
+
+
+def change():
+    if random.random() > 0.5:
+        new_el = random.choice(types_of_els)
+        num_change = random.randint(0, len(els) - 1)
+        while new_el == els[num_change][0]:
+            new_el = random.choice(types_of_els)
+        els[num_change] = (new_el, els[num_change][1], els[num_change][2])
 
 
 def up(scheme, group, x, y, straight):
@@ -267,35 +348,83 @@ def generate(scheme):
 def draw_pare(k_els):
     global scheme1, scheme2
     randomise(k_els)
-    print(els)
-    print(saved_conns)
+    # print(els)
+    # print(saved_conns)
     scheme1 = dict()
     scheme2 = dict()
     generate(scheme1)
+    change()
     generate(scheme2)
-    print_in_file("sch2.txt", merge_schemes(scheme1, scheme2))
-    sys.setrecursionlimit(1000)
-    import Generator_schemes
+    return merge_schemes(scheme1, scheme2)
 
 
-####################################################
-# set data
+def dir_name(path):
+    res = ''
+    for c in path[::-1]:
+        if c == '/':
+            return res
+        res = c + res
+    return res
 
 
-# els = [("lamp", 0, 1), ("switch", 0, 1), ("resister", 0, 2), ("battery", 2, 1)]
-# saved_conns = [{0, 1, 2}, {0, 1, 3}, {2, 3}]
+def choose_directory():
+    global directory, choosed, l_dir
+    directory = fd.askdirectory()
+    l_dir = Label(r, text=dir_name(directory), font=("Comic Sans", 15), bg="#ffdddd", fg="#aa00ff")
+    b_choose.destroy()
+    l_dir.grid(row=0, column=1, sticky=W, pady=10, padx=5)
+    choosed = True
+    r.update()
 
 
-# els = [("lamp", 0, 2), ("switch", 0, 1), ("resister", 1, 5), ("battery", 1, 2),
-#        ("lamp", 1, 2), ("resister", 2, 3), ("switch", 3, 4), ("resister", 4, 5)]
-# saved_conns = [{0, 1},
-#                {1, 2, 3, 4},
-#                {0, 3, 5, 4},
-#                {5, 6},
-#                {6, 7},
-#                {2, 7}]
+def run():
+    if not choosed:
+        mb.showerror("Ошибка", "Выберите файл")
+        return
+    q = int(que.get())
+    k = int(k_els.get())
+    label1.destroy()
+    label2.destroy()
+    label3.destroy()
+    l_dir.destroy()
+    que.destroy()
+    b_work.destroy()
+    k_els.destroy()
+    l_process = Label(r, text="Генерация: 0 из " + str(q), font=("Comic Sans", 15, "bold"), bg="#ffdddd", fg="purple")
+    c = Canvas(r, width=1000, height=40, bg="#ddffdd")
+    l_process.grid(row=0, column=0, padx=10, pady=5, sticky=W)
+    c.grid(row=1, column=0, padx=10, pady=5)
+    r.update()
+    for i in range(q):
+        im = draw(draw_pare(k))
+        im.save(directory + '/' + str(i + 1) + ".png")
+        l_process['text'] = "Генерация: " + str(i + 1) + " из " + str(q)
+        c.create_rectangle(0, 0, int(1000 / q * (i + 1)), 40, fill="#0000dd")
+        r.update()
+    r.destroy()
 
 
-####################################################
 
-draw_pare(6)
+r = Tk()
+r.title("Генератор заданий")
+r['bg'] = "#ffdddd"
+label1 = Label(r, text="Папка для сгенерированных заданий:", font=("Comic Sans", 15, "bold"), bg="#ffdddd", fg="#ff00aa")
+b_choose = Button(r, text="Выбрать", font=("Comic Sans", 15, "bold"), bg="#ffccff", fg="blue",
+                  activeforeground="blue", activebackground="#ffd8ff", command=choose_directory)
+label2 = Label(r, text="Количество заданий:", font=("Comic Sans", 15, "bold"), bg="#ffdddd", fg="#ff00aa")
+que = Spinbox(r, width=10, from_=1, to=1000, font=("Comic Sans", 15))
+label3 = Label(r, text="Количество элементов в схемах:", font=("Comic Sans", 15, "bold"), bg="#ffdddd", fg="#ff00aa")
+k_els = Spinbox(r, width=10, from_=1, to=10, font=("Comic Sans", 15))
+b_work = Button(r, text="Сгенерировать", font=("Comic Sans", 20, "bold"), bg="#ffffcc", fg="orange",
+                  activeforeground="orange", activebackground="#ffffd8", command=run)
+label1.grid(row=0, column=0, sticky=W, pady=10, padx=5)
+b_choose.grid(row=0, column=1, sticky=W, pady=10, padx=5)
+label2.grid(row=1, column=0, sticky=W, pady=10, padx=5)
+que.grid(row=1, column=1, sticky=W, pady=10, padx=5)
+label3.grid(row=2, column=0, sticky=W, pady=10, padx=5)
+k_els.grid(row=2, column=1, sticky=W, pady=10, padx=5)
+b_work.grid(row=3, column=0, columnspan=2, pady=10, padx=5)
+choosed = False
+
+
+r.mainloop()
